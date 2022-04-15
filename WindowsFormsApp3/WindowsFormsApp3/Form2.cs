8665d7fdb33b74b532147d9c4847296bdc95a26e
zekiikose <103570607+zekiikose@users.noreply.github.com>
2022-04-15 12:29:58 +0300

    Login Panel
    Login Panel - SQL Connection

--- WindowsFormsApp3/WindowsFormsApp3/Form2.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form2.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using System.Data;
 
 namespace WindowsFormsApp3
 {
     public partial class Form2 : Form
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
 
         //SqlConnection conn = new SqlConnection("Data Source=NT00938; Initial Catalog=deneme; User Id=zeki.kose;");
 
@@ -22,39 +24,63 @@ namespace WindowsFormsApp3
             InitializeComponent();
         }
 
-        //protected void DatagridYenile()
-        //{
-        //    conn.Open();
-        //    DataTable tbl = new DataTable();
-        //    SqlDataAdapter adptr = new SqlDataAdapter("Select * from user ", conn);
-        //    adptr.Fill(tbl);
-        //    conn.Close();
-            
-        //}
-
-
-
-        private void btnSignin_Click(object sender, EventArgs e)
+        protected void Sign_in()
         {
-            if(txtUsername.Text == "zeki" && txtPassword.Text == "123")
-            {
-                MessageBox.Show("Entry Succes ");
+            string sorgu = "select * from Users where username=@user and password=@pass";
+            con = new SqlConnection("server=NT00938; Initial Catalog=login;Integrated Security=true");
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
 
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Tebrikler! Başarılı bir şekilde giriş yaptınız.");
                 //Diğer formumu açabilmem için o formun classından nesne türetiyorum.
                 Form1 frm = new Form1();
                 //Show metodu ile nesne olarak türettiğim ikinci formumu açıyorum.
                 frm.Show();
                 //Bu mevcut formu kapatıyorum.
                 this.Hide();
-
-
             }
             else
             {
-                MessageBox.Show("Eror!");
+                MessageBox.Show("Kullanıcı adını ve şifrenizi kontrol ediniz.");
             }
-            
+            con.Close();
 
+        }
+
+
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            Sign_in();
+
+            //if(txtUsername.Text == "zeki" && txtPassword.Text == "123")
+            //{
+            //    MessageBox.Show("Entry Succes ");
+
+            //    //Diğer formumu açabilmem için o formun classından nesne türetiyorum.
+            //    Form1 frm = new Form1();
+            //    //Show metodu ile nesne olarak türettiğim ikinci formumu açıyorum.
+            //    frm.Show();
+            //    //Bu mevcut formu kapatıyorum.
+            //    this.Hide();
+
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Eror!");
+            //}
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
